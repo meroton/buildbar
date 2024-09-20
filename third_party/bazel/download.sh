9e8c8aafce9726feeb3eb4bc5b52ceeaaeb598eb
Fredrik Medley <fredrik@meroton.com>
2024-09-20 09:18:54 +0200

    Upgrade to Bazel 8.0.0-pre.20240911.1
    There are new build event messages not available in the 7.3.1 release.

--- third_party/bazel/download.sh
+++ third_party/bazel/download.sh
@@ -13,7 +13,7 @@ fi
 
 bazel_version=$(cat "../../.bazelversion")
 
-curl -L -o bazel-dist.zip "https://github.com/bazelbuild/bazel/releases/download/${bazel_version}/bazel-${bazel_version}-dist.zip"
+curl --fail -L -o bazel-dist.zip "https://github.com/bazelbuild/bazel/archive/refs/tags/${bazel_version}.zip"
 
 function extract_file {
     echo "Extracting $1" 1>&2
@@ -21,12 +21,13 @@ function extract_file {
     unzip -p bazel-dist.zip "$1" > "${output_file}"
 }
 
-extract_file src/main/java/com/google/devtools/build/lib/buildeventstream/proto/build_event_stream.proto
-extract_file src/main/java/com/google/devtools/build/lib/packages/metrics/package_load_metrics.proto
-extract_file src/main/protobuf/action_cache.proto
-extract_file src/main/protobuf/command_line.proto
-extract_file src/main/protobuf/failure_details.proto
-extract_file src/main/protobuf/invocation_policy.proto
-extract_file src/main/protobuf/option_filters.proto
+extract_file "bazel-${bazel_version}/src/main/java/com/google/devtools/build/lib/buildeventstream/proto/build_event_stream.proto"
+extract_file "bazel-${bazel_version}/src/main/java/com/google/devtools/build/lib/packages/metrics/package_load_metrics.proto"
+extract_file "bazel-${bazel_version}/src/main/protobuf/action_cache.proto"
+extract_file "bazel-${bazel_version}/src/main/protobuf/command_line.proto"
+extract_file "bazel-${bazel_version}/src/main/protobuf/failure_details.proto"
+extract_file "bazel-${bazel_version}/src/main/protobuf/invocation_policy.proto"
+extract_file "bazel-${bazel_version}/src/main/protobuf/option_filters.proto"
+extract_file "bazel-${bazel_version}/src/main/protobuf/strategy_policy.proto"
 
 rm bazel-dist.zip
